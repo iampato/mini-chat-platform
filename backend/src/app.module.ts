c1bbddeb95c4707edeaf450f0019b30b9846a596
iampato <858wpwaweru@gmail.com>
2024-08-12 00:45:40 +0300

    added pkgs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,19 @@ import { ChatModule } from './chat/chat.module';
 import { ContactModule } from './contact/contact.module';
 import { PresenceModule } from './presence/presence.module';
 import { ConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PsqlConfigService } from 'src/config/service/psql-config.service';
+import { getAppDataSource } from 'src/typeorm.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: PsqlConfigService) => {
+        return getAppDataSource(configService);
+      },
+      inject: [PsqlConfigService],
+    }),
     AuthModule,
     ChatModule,
     ContactModule,
